4edaa60b4f6336c4305923f0bbbe5b9a431673a7
ldyihe <ldyihe@sina.com>
2017-06-19 09:36:38 +0800

    fd

--- spring/src/main/java/com/huilan/aikf/Test.java
+++ spring/src/main/java/com/huilan/aikf/Test.java
@@ -9,7 +9,7 @@ import org.springframework.core.io.Resource;
 public class Test implements FactoryBean<Object> {
 	
 	public static void main(String[] args) {
-		Resource res = new ClassPathResource("bean.xml");
+		Resource res = new ClassPathResource("bean.xml"); 
 		BeanFactory factory = new XmlBeanFactory(res);  
 		System.out.println(factory.getBean("factoryBeanOne").getClass());
 		System.out.println(factory.getBean("factoryBeanTwo").getClass());
